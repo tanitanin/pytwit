78e362078ced93f3068f15cdb7b8ad8d871a04f2
tanitanin <tanitanin@tanitanin.net>
2012-10-11 19:57:20 +0900

    v1.1に対応

--- pytwit/timeline.py
+++ pytwit/timeline.py
@@ -11,14 +11,16 @@ class TimelineAPI:
   client = None
   
   import hosts
-  api = hosts.API_HOST+"1.1"+"/statuses/"
+  api_url = hosts.API_HOST+"1.1"+"/statuses/"
   
   ''' constractor '''
   def __init__(self, client):
     self.client = client
   
   ''' each API method '''
-  def home_timeline(self,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_rts=None,include_entities=None,exclude_replies=None,contributor_details=None):
+  def home_timeline(self,count=None,since_id=None,max_id=None,page=None,
+                    trim_user=None,include_rts=None,include_entities=None,
+                    exclude_replies=None,contributor_details=None):
     option = {}
     if count: option['count'] = count
     if since_id: option['since_id'] = since_id
@@ -29,11 +31,14 @@ class TimelineAPI:
     if include_entities: option['include_entities'] = include_entities
     if exclude_replies: option['exclude_replies'] = exclude_replies
     if contributor_details: option['contributor_details'] = contributor_details
-    url = self.api + 'home_timeline.json?' + urlencode(option)
+    url = self.api_url + 'home_timeline.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def user_timeline(self,user_id=None,screen_name=None,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_rts=None,include_entities=None,exclude_replies=None,contributor_details=None):
+  def user_timeline(self,user_id=None,screen_name=None,count=None,
+                    since_id=None,max_id=None,page=None,trim_user=None,
+                    include_rts=None,include_entities=None,
+                    exclude_replies=None,contributor_details=None):
     if not user_id and not screen_name: raise Exception
     option = {}
     if user_id: option['user_id'] = user_id
@@ -47,11 +52,13 @@ class TimelineAPI:
     if include_entities: option['include_entities'] = include_entities
     if exclude_replies: option['exclude_replies'] = exclude_replies
     if contributor_details: option['contributor_details'] = contributor_details
-    url = self.api + 'user_timeline.json?' + urlencode(option)
+    url = self.api_url + 'user_timeline.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def mentions(self,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_rts=None,include_entities=None,exclude_replies=None,contributor_details=None):
+  def mentions(self,count=None,since_id=None,max_id=None,page=None,
+               trim_user=None,include_rts=None,include_entities=None,
+               exclude_replies=None,contributor_details=None):
     option = {}
     if count: option['count'] = count
     if since_id: option['since_id'] = since_id
@@ -62,11 +69,12 @@ class TimelineAPI:
     if include_entities: option['include_entities'] = include_entities
     if exclude_replies: option['exclude_replies'] = exclude_replies
     if contributor_details: option['contributor_details'] = contributor_details
-    url = self.api + 'mentions.json?' + urlencode(option)
+    url = self.api_url + 'mentions.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def retweeted_by_me(self,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_entities=None):
+  def retweeted_by_me(self,count=None,since_id=None,max_id=None,page=None,
+                      trim_user=None,include_entities=None):
     option = {}
     if count: option['count'] = count
     if since_id: option['since_id'] = since_id
@@ -74,11 +82,12 @@ class TimelineAPI:
     if page: option['page'] = page
     if trim_user: option['trim_user'] = trim_user
     if include_entities: option['include_entities'] = include_entities
-    url = self.api + 'retweeted_by_me.json?' + urlencode(option)
+    url = self.api_url + 'retweeted_by_me.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def retweeted_to_me(self,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_entities=None):
+  def retweeted_to_me(self,count=None,since_id=None,max_id=None,page=None,
+                      trim_user=None,include_entities=None):
     option = {}
     if count: option['count'] = count
     if since_id: option['since_id'] = since_id
@@ -86,11 +95,12 @@ class TimelineAPI:
     if page: option['page'] = page
     if trim_user: option['trim_user'] = trim_user
     if include_entities: option['include_entities'] = include_entities
-    url = self.api + 'retweeted_to_me.json?' + urlencode(option)
+    url = self.api_url + 'retweeted_to_me.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def retweets_of_me(self,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_entities=None):
+  def retweets_of_me(self,count=None,since_id=None,max_id=None,page=None,
+                     trim_user=None,include_entities=None):
     option = {}
     if count: option['count'] = count
     if since_id: option['since_id'] = since_id
@@ -98,11 +108,13 @@ class TimelineAPI:
     if page: option['page'] = page
     if trim_user: option['trim_user'] = trim_user
     if include_entities: option['include_entities'] = include_entities
-    url = self.api + 'retweets_of_me.json?' + urlencode(option)
+    url = self.api_url + 'retweets_of_me.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def retweeted_by_user(self,user_id=None,screen_name=None,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_entities=None):
+  def retweeted_by_user(self,user_id=None,screen_name=None,count=None,
+                        since_id=None,max_id=None,page=None,trim_user=None,
+                        include_entities=None):
     if not user_id and not screen_name: raise Exception
     option = {}
     if user_id: option['user_id'] = user_id
@@ -113,11 +125,13 @@ class TimelineAPI:
     if page: option['page'] = page
     if trim_user: option['trim_user'] = trim_user
     if include_entities: option['include_entities'] = include_entities
-    url = self.api + 'retweeted_by_user.json?' + urlencode(option)
+    url = self.api_url + 'retweeted_by_user.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
-  def retweeted_to_user(self,user_id=None,screen_name=None,count=None,since_id=None,max_id=None,page=None,trim_user=None,include_entities=None):
+  def retweeted_to_user(self,user_id=None,screen_name=None,count=None,
+                        since_id=None,max_id=None,page=None,trim_user=None,
+                        include_entities=None):
     if not user_id and not screen_name: raise Exception
     option = {}
     if user_id: option['user_id'] = user_id
@@ -128,19 +142,19 @@ class TimelineAPI:
     if page: option['page'] = page
     if trim_user: option['trim_user'] = trim_user
     if include_entities: option['include_entities'] = include_entities
-    url = self.api + 'retweeted_to_user.json?' + urlencode(option)
+    url = self.api_url + 'retweeted_to_user.json?' + urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
   
 #######
   def public_timeline(self,option):
-    url = self.api+'public_timeline.json?'+urlencode(option)
+    url = self.api_url+'public_timeline.json?'+urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
   
   def friends_timeline(self,option):
-    url = self.api+'friends_timeline.json?'+urlencode(option)
+    url = self.api_url+'friends_timeline.json?'+urlencode(option)
     st,res = self.client.request(url,'GET')
     return st,json.loads(res)
 
